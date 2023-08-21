d160749de7d2bde90f3a81afc03f9620300ad184
Matache Razvan <rmatache@delloittece.com>
2023-08-21 14:05:11 +0300

    [RM] added JavaSwitchExercise

--- src/main/java/org/example/TestClass.java
+++ src/main/java/org/example/TestClass.java
@@ -1,6 +1,27 @@
-package org.example;//public class TestClass {
-////    public static void main(String[] args) throws Exception { boolean flag = true;
-////        switch (flag){
-////            case true : System.out.println("true");
-////            default: System.out.println("false"); }
-////    } }
+public class TestClass {
+
+    public static void main(String[] args) throws Exception { boolean flag = true;
+        switch (flag){
+            case true : System.out.println("true");
+            default: System.out.println("false"); }
+    } }
+
+//    public void switchString(String input) {
+////        switch (input) {
+////            case "a":
+////                System.out.println("apple");
+////            case "b":
+////                System.out.println("bat");
+////                break;
+////            case "B":
+////                System.out.println("big bat");
+////            default:
+////                System.out.println("none");
+////        }
+////    }
+////
+////    public static void main(String[] args) throws Exception {
+////        TestClass tc = new TestClass();
+////        tc.switchString("B");
+////    }
+////}

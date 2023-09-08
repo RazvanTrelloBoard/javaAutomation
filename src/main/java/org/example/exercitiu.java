38b74224a9588893d63d23368cadec0b10738933
Matache Razvan <rmatache@delloittece.com>
2023-09-08 10:12:38 +0300

    [RM] exercitiu

---
+++ src/main/java/org/example/exercitiu.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class exercitiu {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 0;
+        int c = b + a;
+        System.out.println(b=c);
+    }
+}
